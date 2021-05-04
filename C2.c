87be650e71ebac623fb359d0921122b87019892a
S.Rohith <tanukusai@hotmail.com>
2021-05-04 21:06:44 +0530

    first commit

---
+++ C2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char a[100],b[100],c[100];
+    gets(a);
+  
+    gets(b);
+
+    gets(c);
+
+    puts(a);
+
+    puts(b);
+
+    puts(c);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
---
+++ C3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a,b;
+    float c,d;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%f",&c);
+    scanf("%f",&d);
+    printf("%d",a+b);
+    printf("%d\n",a-b); 
+    printf("%f",c+d);
+    printf("%f",c-d);   
+    
+}
